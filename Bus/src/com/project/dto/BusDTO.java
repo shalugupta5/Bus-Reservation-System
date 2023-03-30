f3a40e9b83b911d44739372a44986aa998debcc0
shalu <shalug123456@gmail.com>
2023-03-30 23:11:19 +0530

    day-2

---
+++ Bus/src/com/project/dto/BusDTO.java
@@ -0,0 +1,21 @@
+package com.project.dto;
+
+public interface BusDTO {
+	
+	
+
+	public String getBusNumber();
+	
+	public void setBusNumber(String busNumber);
+	
+	public String getBusType() ;
+	
+	public void setBusType(String busType);
+	
+	public int getTotalSeats();
+	
+	public void setTotalSeats(int totalSeats);
+
+	
+	
+}
